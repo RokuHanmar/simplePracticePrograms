ef8865fc535b704d4aff9ffb373cd52f529cab73
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-12-11 16:59:52 +0000

    Create Mad Libs.php

---
+++ PHP/Mad Libs.php
@@ -0,0 +1,15 @@
+<?php
+
+function generateStory($singular_noun, $verb, $colour, $distance_unit) {
+  $story = "\nThe ". $singular_noun . " are lovely, ". $colour. ", and deep.\n
+But I have promises to keep,\n
+And ". $distance_unit. " to go before I ". $verb. ",\n
+And ". $distance_unit. " to go before I ". $verb. ".\n
+";
+
+  return $story;
+}
+
+echo generateStory("man", "run", "red", "metres");
+echo generateStory("woman", "jog", "blue", "furlongs");
+echo generateStory("shild", "swim", "green", "inches");
