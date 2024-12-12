7b53f23987cc453bf4c05cbd528df03af0b4120b
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-12-12 16:21:32 +0000

    Update Binary Search.js
    Mostly works, just needs to be able to handle missing cases

--- JavaScript/Binary Search.js
+++ JavaScript/Binary Search.js
@@ -1,31 +1,30 @@
 const search = (array, target) => {
-  let found = false;
-  let startpoint = array[0];
-  let endpoint = array[array.length - 1];
-  
+    let found = false;
+    let startpoint = 0;
+    let endpoint = array.length - 1;
 
-/*  console.log(startpoint);
-  console.log(endpoint);
-  console.log(midpoint);*/
 
-  while (found === false && array.length > 0) {
-    let midpoint = array[Math.floor(array.length / 2)];
-    if (midpoint === target) {
-      console.log(`Found at position ${midpoint}`);
-      found = true;
-    
-    } else if (midpoint > target) {
-      endpoint = midpoint;
-    
-    } else if (midpoint < target) {
-      startpoint = midpoint;
+    /*console.log(startpoint);
+      console.log(endpoint);
+      console.log(midpoint);*/
+
+    while (startpoint <= endpoint && found === false) {
+        let midpoint = Math.floor((startpoint + endpoint) / 2);
+        if (array[midpoint] === target) {
+            console.log(`Found at position ${midpoint}`);
+            found = true;
+
+        } else if (array[midpoint] > target) {
+            endpoint = midpoint;
+
+        } else if (array[midpoint] < target) {
+            startpoint = midpoint;
+        }
     }
-  }
 
-  if (array.length <= 0 && found === false) {
-    console.log("Target not in array");
-    return null;
-  }
+    if (startpoint >= endpoint && found === false) {
+        console.log("Target not in array");
+    }
 }
 
 search([1,2,3,4,5], 1); // returns found at 0
